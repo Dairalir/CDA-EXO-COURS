9c54796420f9d0dd67790b791717394a649b5b56
Dairalir <daimanvm@gmail.com>
2023-03-13 16:52:02 +0100

    declencheur exo

--- BDD/declencheur/declencheur.sql
+++ BDD/declencheur/declencheur.sql
@@ -72,3 +72,33 @@ CREATE TRIGGER insert_chambre AFTER INSERT ON chambre
             SIGNAL SQLSTATE '40000' SET MESSAGE_TEXT = 'Un problème est survenu. capatité chambre supérieur a 50 !';
         END IF;
 END|
+
+/**/
+
+INSERT INTO lignedecommande(id_commande, id_produit, quantite, prix)
+VALUES
+(1, 1, 3, 10)
+
+DELIMITER |
+
+CREATE TRIGGER maj_total AFTER INSERT ON lignedecommande
+    FOR EACH ROW
+    BEGIN
+        DECLARE id_c INT;
+        DECLARE tot DOUBLE;
+        SET id_c = NEW.id_commande; -- nous captons le numéro de commande concerné
+        SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c); -- on recalcul le total
+        UPDATE commande SET total=tot WHERE id=id_c; -- on stocke le total dans la table commande
+END|
+
+DELIMITER |
+
+CREATE TRIGGER maj_total AFTER INSERT ON lignedecommande
+    FOR EACH ROW
+    BEGIN
+        DECLARE id_c INT;
+        DECLARE tot DOUBLE;
+        SET id_c = NEW.id_commande; -- nous captons le numéro de commande concerné
+        SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c); -- on recalcul le total
+        UPDATE commande SET total=tot WHERE id=id_c; -- on stocke le total dans la table commande
+END|
